9fb4a0d6e6b98a7bc8eeeda14a97795bc6232256
karthickr <karthickravi0297@gmail.com>
2024-06-30 18:41:33 +0530

    contact manager app initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1>Contact Manager</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
